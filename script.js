da99a9366bfed140abe129dc6a7e39a7dc7dc102
ToniEngine <antoineorbot@gmail.com>
2026-01-21 14:12:08 +0100

    updated the contact

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ if (resumeBtn) {
         e.preventDefault();
         
         // Replace 'Anthony_Obot_Resume.pdf' with your actual resume filename
-        // const resumeFileName = 'Anthony_Obot_Resume.pdf';
+        const resumeFileName = 'Anthony_Obot_Resume.pdf';
+
         
         // Create a temporary link element
         const link = document.createElement('a');
